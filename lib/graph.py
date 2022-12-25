99340116a4bb50530e5bbd181db74b74c2351eac
Matt Billenstein <matt@vazor.com>
2022-12-24 16:30:40 -0800

    Notes on graphs

---
+++ lib/graph.py
@@ -0,0 +1,10 @@
+# I really need a set of canned graph algorithms I can rely on - reinventing
+# the wheel every problem is killing a lot of nights...
+
+# https://www.reddit.com/r/adventofcode/comments/zu7kq8/advice_maybe_abstract_your_searches/
+# https://www.reddit.com/r/adventofcode/comments/zubqho/2022_day_24_why_bfs_not_dfs/
+
+# bfs - shortest path
+# dfs - find a path, need a lot of pruning probably...
+# djikstra - 
+# astar?
