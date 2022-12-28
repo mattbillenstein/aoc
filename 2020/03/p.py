7b54d961f77401715e01dfae1926bf56259ad4b3
Matt Billenstein <matt@vazor.com>
2022-12-27 16:42:12 -0800

    2020 Day 3

---
+++ 2020/03/p.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env pypy3
+
+import sys
+import time
+from collections import defaultdict
+from pprint import pprint
+
+from grid import Grid
+
+DEBUG = '--debug' in sys.argv
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
+
+def parse_input():
+    lines = [_.strip('\r\n') for _ in sys.stdin]
+    g = Grid(lines)
+    return g
+
+def count_along_slope(grid, dx, dy):
+    if DEBUG:
+        grid.print()
+        print(grid.size)
+
+    cnt = 0
+    for y in range(0, grid.size[1], dy):
+        if y == 0:
+            x = 0
+        else:
+            x = (x + dx) % grid.size[0]
+
+        if DEBUG:
+            print(x, y, dx, dy, grid.get((x, y)))
+        if grid.get((x, y)):
+            cnt += 1
+
+    return cnt
+        
+def part1(grid):
+    print(count_along_slope(grid, 3, 1))
+
+def part2(data):
+    p = 1
+    for dx, dy in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        cnt = count_along_slope(data, dx, dy)
+        p *= cnt
+    print(p)
+
+def main():
+    data = parse_input()
+    if '1' in sys.argv:
+        part1(data)
+    if '2' in sys.argv:
+        part2(data)
+
+if __name__ == '__main__':
+    main()
--- 2020/README.txt
+++ 2020/README.txt
@@ -1,2 +1,3 @@
 01: Product of combinations that sum to 2020
 02: Check list of passwords given simple rules
+03: Counting set grid points along a diagonal slope
