ac4c96989f3ab269add9b651624a09b9e8adb06b
Matt Billenstein <matt@vazor.com>
2022-12-29 01:17:18 -0800

    2020 Day 11

--- 2020/11/p.py
+++ 2020/11/p.py
@@ -39,8 +39,37 @@ def part1(grid):
     cnt = sum(1 if grid.get(_) == 2 else 0 for _ in grid)
     print(cnt)
 
-def part2(data):
-    pass
+def scan8(grid, pos):
+    for dir in ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'):
+        p2 = pos
+        while 1:
+            p2 = grid.step(p2, dir)
+            if p2 is None:
+                break
+            if grid.get(p2):
+                yield p2
+                break
+
+def part2(grid):
+    while 1:
+        g = grid.copy()
+        changed = False
+        for pos in grid:
+            v = grid.get(pos)
+            if v == 1 and all(grid.get(_) != 2 for _ in scan8(grid, pos)):
+                g.set(pos, 2)
+                changed = True
+            elif v == 2 and sum(1 if grid.get(_) == 2 else 0 for _ in scan8(grid, pos)) >= 5:
+                g.set(pos, 1)
+                changed = True
+
+        grid = g
+
+        if not changed:
+            break
+
+    cnt = sum(1 if grid.get(_) == 2 else 0 for _ in grid)
+    print(cnt)
 
 def main():
     data = parse_input()
