ee30eaed3a287a03cd48924825bcffc5a10a11cb
Matt Billenstein <matt@vazor.com>
2022-12-30 19:57:29 -0800

    Get input based on directory

--- get-input.sh
+++ get-input.sh
@@ -4,12 +4,8 @@ set -eo pipefail
 
 COOKIE="$(cat ~/src/mattb/dotfiles/aoc)"
 
-YEAR="$(date --utc +'%Y')"
-DAY="$(date --utc +'%_d' | tr -d ' ')"
-if [ "$1" != "" ]; then
-  YEAR="$1"
-  DAY="$2"
-fi
+YEAR="$(basename $(dirname $PWD))"
+DAY="$(basename $PWD | sed -e 's/^0//')"
 
 UA="github.com/mattbillenstein/aoc/get-data.sh v0 by matt@vazor.com"
 curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$DAY/input > input.txt
