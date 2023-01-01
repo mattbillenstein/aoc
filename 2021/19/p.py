6cd7e943d501e1da6b2d89a77696ed5f5dbed162
Matt Billenstein <matt@vazor.com>
2023-01-01 11:01:57 -0800

    Working on p1 test with some hand-coding

--- 2021/19/p.py
+++ 2021/19/p.py
@@ -32,7 +32,7 @@ def parse_input():
 
     return scanners
 
-def mdist(pt1, pt2):
+def adist(pt1, pt2):
     return (abs(pt1[0] - pt2[0]), abs(pt1[1] - pt2[1]), abs(pt1[2] - pt2[2]))
 
 def dist(pt1, pt2):
@@ -82,73 +82,85 @@ def part1(scanners):
     for num, beacons in scanners.items():
         for b1, b2 in permutations(beacons, 2):
             # insert sorted so later matched points are ordered...
-            tup = (b2, mdist(b1, b2))
+            tup = (b2, adist(b1, b2))
             distances[(b1, num)].append(tup)
 
-    print()
-    pprint(distances)
-    print()
+    if DEBUG:
+        print('DISTANCE')
+        pprint(distances)
+        print()
 
     matched = defaultdict(list)
     for k, L in distances.items():
         b1, num = k
         L.sort(key=lambda x: sum(x[1]))
-        print(sum(L[0][1]), sum(L[1][1]), k[1], k[0], L[0], L[1])
 
         # now key by nearest two manhattan distances
-        nk = tuple(sorted([tuple(sorted(L[0][1])), tuple(sorted(L[1][1])), tuple(sorted(L[2][1]))]))
-        matched[nk].append((num, b1, L[0][0], L[1][0], L[2][0]))
+        nk = tuple(sorted([tuple(sorted(L[0][1])), tuple(sorted(L[1][1]))]))
+        matched[nk].append((num, b1, L[0][0], L[1][0]))
 
-    print()
-    pprint(matched)
-    print()
+    if DEBUG:
+        print('MATCHED')
+        pprint(matched)
+        print()
 
     translate = {}
     for k, L in matched.items():
         if len(L) > 1:
-            print(f'Relate: {[_[0] for _ in L]} {k}')
-            print(L)
-            num, p1, p2, p3, p4 = L[0]
+            debug(f'Relate: {[_[0] for _ in L]} {k}')
+            debug(L)
+            num, p1, p2, p3 = L[0]
             dist1 = dist(p1, p2)
             dist2 = dist(p1, p3)
-            dist3 = dist(p1, p4)
-            print(num, dist1, dist2, dist3)
-            for onum, op1, op2, op3, op4 in L[1:]:
+            debug(num, dist1, dist2)
+            for onum, op1, op2, op3 in L[1:]:
                 odist1 = dist(op1, op2)
                 odist2 = dist(op1, op3)
-                odist3 = dist(op1, op4)
-                print(onum, odist1, odist2, odist3)
+                debug(onum, odist1, odist2)
 
                 # now figure out which swap/flip matches, compute offset, and
                 # store those...
                 for swp in SWAPS:
                     for flp in FLIPS:
                         if translate_point(odist1, swp, flp) == dist1 \
-                            and translate_point(odist2, swp, flp) == dist2 \
-                            and translate_point(odist3, swp, flp) == dist3:
+                            and translate_point(odist2, swp, flp) == dist2:
 
                             tp1 = translate_point(op1, swp, flp)
                             offset = (p1[0] - tp1[0], p1[1] - tp1[1], p1[2] - tp1[2])
-#                            roffset = (tp1[0] - p1[0], tp1[1] - p1[1], tp1[2] - p1[2])
 
                             assert translate_point(op1, swp, flp, offset) == p1
                             assert translate_point(op2, swp, flp, offset) == p2
-                            assert translate_point(op3, swp, flp, offset) == p3
-
-                            print(reverse_translate_point(translate_point(op1, swp, flp, offset), swp, flp, offset), op1)
 
                             assert reverse_translate_point(translate_point(op1, swp, flp, offset), swp, flp, offset) == op1
                             assert reverse_translate_point(translate_point(op2, swp, flp, offset), swp, flp, offset) == op2
-                            assert reverse_translate_point(translate_point(op3, swp, flp, offset), swp, flp, offset) == op3
 
                             assert (num, onum) not in translate or translate[(num, onum)] == (swp, flp, offset), ('Mismatch', translate[(num, onum)], (swp, flp, offset))
-                            translate[(num, onum)] = (swp, flp, offset)
-                            print(f'Match {num} -> {onum}', swp, flp, offset)
+                            translate[(onum, num)] = (swp, flp, offset)
+                            debug(f'Match {num} -> {onum}', swp, flp, offset)
 
 
-            print()
+    if 1 or DEBUG:
+        print('TRANSLATE')
+        pprint(translate)
 
-    pprint(translate)
+    beacons = set()
+    for b in scanners[0]:
+        beacons.add(b)
+
+    for src in (1, 2, 4):
+        for b in scanners[src]:
+            b = translate_point(b, *translate[(src, 0)])
+            beacons.add(b)
+
+    for b in scanners[3]:
+        b = translate_point(b, *translate[(3, 1)])
+        b = translate_point(b, *translate[(1, 0)])
+        beacons.add(b)
+
+    for b in sorted(beacons):
+        debug(b)
+
+    print(len(beacons))
 
 def part2(data):
     pass
