3f53336ec919011dc5a9b54f48a60c33e083ad95
Matt Billenstein <matt@vazor.com>
2023-02-02 12:49:10 -0800

    Checkpoint slow bfs implementation

--- 2019/18/p.py
+++ 2019/18/p.py
@@ -2,6 +2,7 @@
 
 import itertools
 import math
+import random
 import sys
 import time
 from collections import defaultdict
@@ -61,40 +62,56 @@ def bfs(frontier, grid, end=None):
 
         distance += 1
 
-    found.sort(key=lambda x: x[2])
-    return found
+last = time.time()
+bdist = sys.maxsize
+best = {}
+def find_keys(grid, pos, keys, all_keys, dist):
+    global last, best, bdist
 
-def find_keys(grid, pos, keys, all_keys, dist, bdist):
-    if keys == all_keys:
-        return dist
-
-    if DEBUG:
-        print(pos, keys, dist)
-        grid.print()
+    fs = frozenset(keys)
+    if best.get((pos, fs), sys.maxsize) < dist:
+        return None
 
     # recurse until we find all keys
-    found = bfs(pos, grid)
+    if keys == all_keys:
+        if dist < bdist:
+            bdist = dist
+        return dist
 
-    if DEBUG:
+    doprint = time.time() - last > 10
+    if DEBUG or doprint:
+        last = time.time()
+        grid.print()
+        print(pos, keys, dist, bdist)
+
+    found = bfs(pos, grid)
+    found.sort(key=lambda x: x[2])
+
+    # if dist + the distance to the furthest key/door > bdist continue
+    maxfound = max(_[2] for _ in found)
+    if dist + maxfound > bdist:
+        return dist + maxfound
+
+    if DEBUG or doprint:
         print(found)
         print()
 
     mdist = bdist
     for pt, c, fdist in found:
-        if dist + fdist > mdist:
+        if dist + fdist > bdist:
             continue
 
         if 'a' <= c <= 'z':
             grid.set(pt, 0)
             keys.add(c)
-            ndist = find_keys(grid, pt, keys, all_keys, dist + fdist, bdist)
-            if ndist < mdist:
+            ndist = find_keys(grid, pt, keys, all_keys, dist + fdist)
+            if ndist is not None and ndist < mdist:
                 mdist = ndist
             keys.remove(c)
             grid.set(pt, ord(c))
         elif 'A' <= c <= 'Z' and c.lower() in keys:
             grid.set(pt, 0)
-            ndist = find_keys(grid, pt, keys, all_keys, dist + fdist, bdist)
+            ndist = find_keys(grid, pt, keys, all_keys, dist + fdist)
             if ndist < mdist:
                 mdist = ndist
             grid.set(pt, ord(c))
@@ -113,8 +130,12 @@ def part1(grid):
             grid.set(pt, 0)
         elif 'a' <= c <= 'z':
             all_keys.add(c)
+#            grid.set(pt, 0)
+#            found = bfs(pt, grid)
+#            grid.set(pt, v)
+#            print(c, pt, found)
 
-    dist = find_keys(grid, pos, set(), all_keys, 0, sys.maxsize)
+    dist = find_keys(grid, pos, set(), all_keys, 0)
     print(dist)
 
 def part2(data):
