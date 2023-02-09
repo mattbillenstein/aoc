c74babe0aa586cca58ca781ab8bbe88578236eff
Matt Billenstein <matt@vazor.com>
2023-02-09 07:00:37 -0800

    2018 Day 2

---
+++ 2018/02/p.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env pypy3
+
+import itertools
+import math
+import sys
+import time
+from collections import defaultdict
+from pprint import pprint
+
+DEBUG = '--debug' in sys.argv
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
+
+def parse_input():
+    lines = [_.strip('\r\n') for _ in sys.stdin]
+    return lines
+
+def part1(data):
+    cnt2 = 0
+    cnt3 = 0
+    for line in data:
+        d = defaultdict(int)
+        for c in line:
+            d[c] += 1
+
+        if 2 in d.values():
+            cnt2 += 1
+        if 3 in d.values():
+            cnt3 += 1
+
+    print(cnt2 * cnt3)
+
+def part2(data):
+    for s1 in data:
+        for s2 in data:
+            if s1 == s2:
+                continue
+            diff = 0
+            s = ''
+            for c1, c2 in zip(s1, s2):
+                if c1 != c2:
+                    diff += 1
+                else:
+                    s += c1
+
+            if diff == 1:
+                debug(s1, s2)
+                print(s)
+                return
+
+def main():
+    data = parse_input()
+    if '1' in sys.argv:
+        part1(data)
+    if '2' in sys.argv:
+        part2(data)
+
+if __name__ == '__main__':
+    main()
--- 2018/README.txt
+++ 2018/README.txt
@@ -1,1 +1,2 @@
 01: Simple addition of numbers in list mod length of list
+02: Count differences in strings
