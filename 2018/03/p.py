c34251bd420f3ebe9a8f114b5dac2440091df415
Matt Billenstein <matt@vazor.com>
2023-02-09 07:57:17 -0800

    2018 Day 3

---
+++ 2018/03/p.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env pypy3
+
+import itertools
+import math
+import sys
+import time
+from collections import defaultdict
+from pprint import pprint
+
+DEBUG = '--debug' in sys.argv
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
+
+def parse_input():
+    lines = [_.strip('\r\n') for _ in sys.stdin]
+    claims = []
+    for line in lines:
+        for c in '#@,:x':
+            line = line.replace(c, ' ')
+        tup = [int(_) for _ in line.strip().split()]
+        claims.append({'id': tup[0], 'loc': (tup[1], tup[2]), 'size': (tup[3], tup[4])})
+    return claims
+
+def part(data):
+    d = defaultdict(int)
+    for claim in data:
+        loc = claim['loc']
+        size = claim['size']
+        for x in range(loc[0], loc[0] + size[0]):
+            for y in range(loc[1], loc[1] + size[1]):
+                d[(x, y)] += 1
+
+    print(sum(1 for _ in d.values() if _ >= 2))
+
+    for claim in data:
+        loc = claim['loc']
+        size = claim['size']
+        cnt = 0
+        for x in range(loc[0], loc[0] + size[0]):
+            for y in range(loc[1], loc[1] + size[1]):
+                if d[(x, y)] > 1:
+                    cnt += 1
+        if not cnt:
+            print(claim['id'])
+            break
+
+def main():
+    data = parse_input()
+    part(data)
+
+if __name__ == '__main__':
+    main()
--- 2018/README.txt
+++ 2018/README.txt
@@ -1,2 +1,3 @@
 01: Simple addition of numbers in list mod length of list
 02: Count differences in strings
+03: Counting overlapping rectangles
