7a7ec0ac64bbf40fd73f7ac396489a219ef069f6
Matt Billenstein <matt@vazor.com>
2023-02-12 16:21:41 -0800

    Checkpoint

--- 2018/17/p.py
+++ 2018/17/p.py
@@ -32,7 +32,7 @@ def parse_input():
             x0 = int(L[3])
             x1 = int(L[4])
 
-        if x0 < 550 and y0 < 150:
+        if 1 or x0 < 570 and y0 < 150:
             for x in range(x0, x1 + 1):
                 for y in range(y0, y1 + 1):
                     g.setc((x, y), '#')
@@ -40,82 +40,100 @@ def parse_input():
     return g
 
 def drop(pt, grid, maxy):
-    if pt[1] > maxy:
-        return None
+    while 1:
+        npt = grid.step(pt, 'v')
+        if npt[1] > maxy:
+            return None
 
-    npt = grid.step(pt, 'v')
-    c = grid.getc(npt)
-    if c == '.':
+        c = grid.getc(npt)
+        if c not in '.|':
+            break
+
         grid.setc(npt, '|')
-        return npt
+
+        pt = npt
 
     return pt
 
-def fill(startpt, grid):
-    drops = []
+def fill(startpt, grid, srcs):
+    # check we're on something...
+    assert grid.get(grid.step(startpt, 'v'), 0)
 
-    filled = []
+    scan = {}
 
     for dir in '<>':
         pt = startpt
         while 1:
-            if grid.getc(grid.step(pt, 'v')) == '.':
-                drops.append(pt)
-                break
-
             npt = grid.step(pt, dir)
             c = grid.getc(npt)
-            if c in '.|':
-                grid.setc(npt, '|')
-                filled.append(npt)
+            if c == '#':
+                scan[dir] = ('wall', pt)
+                break
+
+            elif c in '.|':
+                if grid.getc(grid.step(npt, 'v')) in '.|':
+                    scan[dir] = ('drop', npt)
+                    break
                 pt = npt
-            elif c == '#':
-                break
-            elif c == '~':
-                break
 
-    if not drops:
-        for pt in filled + [startpt]:
-            grid.setc(pt, '~')
+    L, R = scan['<'], scan['>']
+    c = '|'
+    if L[0] == R[0] == 'wall':
+        c = '~'
 
-    return drops
+    y = L[1][1]
+    for x in range(L[1][0], R[1][0]+1):
+        pt = (x, y)
+        grid.setc(pt, c)
+        if c == '~' and pt in srcs:
+            srcs.remove(pt)
 
+    for tup in (L, R):
+        if tup[0] == 'drop':
+            srcs.add(tup[1])
+        
 def part1(grid):
     faucet = (500, 0)
     grid.setc(faucet, '+')
 
-    grid.print()
-
     maxy = max(_[1] for _ in grid)
 
-    drops = []
-    ndrops = [faucet]
+    srcs = set([faucet])
 
     while 1:
-        for pt in ndrops:
-            while 1:
-                npt = drop(pt, grid, maxy)
-                if npt is None or npt == pt:
-                    break
+        g = grid.copy()
 
-                drops.append(npt)
-                pt = npt
-
-        while drops:
-            ndrops = fill(drops[-1], grid)
-
+        if DEBUG:
             print()
-            print(ndrops)
-            print(drops)
             grid.print()
 
-            if ndrops:
-                break
+        for pt in list(srcs):
+            if pt not in srcs:
+                continue
 
-            drops.pop()
+            pt = drop(pt, grid, maxy)
+            if pt is None:
+                continue
 
-        if all(_[1] > maxy for _ in ndrops):
+            fill(pt, grid, srcs)
+
+        if g.g == grid.g:
             break
+
+    if 1 or DEBUG:
+        print()
+        grid.print()
+
+    cnt = 0
+    for pt in grid:
+        if pt[1] > maxy:
+            continue
+
+        c = grid.getc(pt)
+        if c in '~|':
+            cnt += 1
+
+    print(cnt)
 
 def part2(data):
     pass
