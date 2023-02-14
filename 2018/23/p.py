a6fa132b00dabd99dc183220a9780aa9c23451f9
Matt Billenstein <matt@vazor.com>
2023-02-14 15:03:17 -0800

    Real solution for 2018/23

--- 2018/23/p.py
+++ 2018/23/p.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env pypy3
 
-import itertools
-import math
-import random
 import sys
-import time
 from collections import defaultdict
-from pprint import pprint
 
 DEBUG = sys.argv.count('-v')
 
@@ -29,9 +24,6 @@ def parse_input():
 def manhattan_distance(a, b):
     return sum(abs(x-y) for x, y in zip(a, b))
 
-def count_in_range(pt, bots):
-    return sum(1 for b, r in bots if manhattan_distance(pt, b) <= r)
-
 def part1(bots):
     bots.sort(key=lambda x: x[1])
 
@@ -44,97 +36,96 @@ def part1(bots):
 
     print(cnt)
 
+def box_intersect(box, bot, r):
+    # this little bit is cribbed - this manhattan distance intersection
+    # requires a bit more study...
+    d = 0
+    for i in range(3):
+        low, high = box[0][i], box[1][i] - 1
+        d += abs(bot[i] - low) + abs(bot[i] - high)
+        d -= high - low
+    d //= 2
+    return d <= r
+
+def count_in_range(box, bots):
+    return sum(1 for b, r in bots if box_intersect(box, b, r))
+
+def split_box(box):
+    x0, y0, z0 = box[0]
+    x2, y2, z2 = box[1]
+    x1 = x0 + (x2 - x0) // 2
+    y1 = y0 + (y2 - y0) // 2
+    z1 = z0 + (z2 - z0) // 2
+    return [
+        ((x0, y0, z0), (x1, y1, z1)),
+        ((x0, y0, z1), (x1, y1, z2)),
+        ((x0, y1, z0), (x1, y2, z1)),
+        ((x1, y0, z0), (x2, y1, z1)),
+        ((x0, y1, z1), (x1, y2, z2)),
+        ((x1, y1, z0), (x2, y2, z1)),
+        ((x1, y0, z1), (x2, y1, z2)),
+        ((x1, y1, z1), (x2, y2, z2)),
+    ]
+
+def box_size(box):
+    return box[1][0] - box[0][0]
+
 def part2(bots):
-    # Iteratively try to move in range of an out of range point keeping the
-    # best point we've found along the way...
-    #
-    # This never exits, let it run and try what it outputs, should find a
-    # solution in a few minutes...
-
-    # Here are some of them with the same manhattan distance::
-    # (54127929, 16826164, 30645449) 945 101599542
-    # (54127928, 16826164, 30645448) 947 101599540 *
-    # (54144077, 17007607, 30447855) 948 101599539
-    # (54144079, 17007607, 30447854) 952 101599540 *
-    # (54127927, 17023734, 30447877) 966 101599538
-    # (54127928, 17023741, 30447871) 971 101599540 *
-    # (54127928, 17023747, 30447866) 972 101599541
-    # (54127929, 17023757, 30447857) 973 101599543
-    # (54127928, 17023756, 30447856) 974 101599540 *
-    # (54127928, 17023757, 30447855) 977 101599540 *
-    # (54127928, 17023757, 30447854) 977 101599539
-
-    start = time.time()
-
+    # box solution
     origin = (0, 0, 0)
 
-    # start at an average point among all the bots
-    pt = (
-        sum(b[0] for b, r in bots) // len(bots),
-        sum(b[1] for b, r in bots) // len(bots),
-        sum(b[2] for b, r in bots) // len(bots),
-    )
-    score = count_in_range(pt, bots)
-    dist = manhattan_distance(origin, pt)
+    # make a large box in range of all bots
+    size = 1
+    while 1:
+        box = ((-size, -size, -size), (size, size, size))
+        if count_in_range(box, bots) == len(bots):
+            break
+        size *= 2
 
-    print(pt, score, dist, time.time() - start)
-
-    lastpt = None
+    # split the box into 8 octants and keep the best scoring ones by size - as
+    # the boxes get smaller, they will be in range of less bots...
+    best = defaultdict(list)
+    best[box_size(box)].append((count_in_range(box, bots), box))
 
     while 1:
-        # if point changed, recompute list of out of range points
-        if pt != lastpt:
-            lastpt = pt
-            L = [(r - manhattan_distance(pt, b), b, r) for b, r in bots]
-            L = [tup for tup in L if tup[0] < 0]
-            L.sort(reverse=True)
+        # examine all boxes with the same score at the smallest size
+        size = min(best)
+        boxes = best[size]
+        boxes.sort(reverse=True)
+        boxes = [_[1] for _ in boxes if _[0] == boxes[0][0]]
 
-        # pick a random closer one
-        d, b, r = random.choice(L)
-        d = abs(d)
+        # small enough, just spin through all left points
+        if size < 8:
+            break
 
-        # try random points on the surface
-        bad = 0
-        for i in range(1000):
-            # randomly divide manhattan distance away among each axis
-            dx = min(d, abs(pt[0]-b[0]))
-            px = random.randrange(0, dx)
+        for box in boxes:
+            debug(box_size(box), box, count_in_range(box, bots))
+            for b in split_box(box):
+                size = box_size(b)
+                cnt = count_in_range(b, bots)
+                debug('  ', box_size(b), b, count_in_range(b, bots))
+                best[size].append((cnt, b))
 
-            dy = min(d-px, abs(pt[1]-b[1]))
-            py = random.randrange(0, dy)
+    # now brute force search all points in remaining boxes keeping point with
+    # best score and lowest manhattan distance to origin...
+    pt = None
+    cnt = 0
+    dist = 0
+    for box in boxes:
+        debug(box_size(box), box, count_in_range(box, bots))
 
-            pz = d - px - py
+        for x in range(box[0][0], box[1][0]):
+            for y in range(box[0][1], box[1][1]):
+                for z in range(box[0][2], box[1][2]):
+                    npt = (x, y, z)
+                    ndist = manhattan_distance(origin, npt)
+                    ncnt = sum(1 for b, r in bots if manhattan_distance(npt, b) <= r)
+                    if ncnt > cnt or (ncnt == cnt and ndist < dist):
+                        pt = npt
+                        cnt = ncnt
+                        dist = ndist
 
-            if pt[0] > b[0]:
-                px = -px
-            if pt[1] > b[1]:
-                py = -py
-            if pt[2] > b[2]:
-                pz = -pz
-
-            npt = (pt[0] + px, pt[1] + py, pt[2] + pz)
-
-            ndist = manhattan_distance(b, npt)
-            if ndist > r:
-                # d remainder overflowed pz, just throw in a random point
-                step = max(i, 1)
-                npt = (
-                    random.randrange(pt[0] - step, pt[0] + step),
-                    random.randrange(pt[1] - step, pt[1] + step),
-                    random.randrange(pt[1] - step, pt[1] + step),
-                )
-
-            cnt = count_in_range(npt, bots)
-            mdist = manhattan_distance(origin, npt)
-            if cnt > score or (cnt == score and mdist < dist):
-                score = cnt
-                dist = mdist
-                pt = npt
-
-                print(pt, score, dist, time.time() - start)
-
-                # point changed, recompute L
-                break
+    print(pt, cnt, dist)
 
 def main():
     data = parse_input()
