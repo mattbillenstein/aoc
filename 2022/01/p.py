4bf76983354f029e0187b4f613e211b7a3d21a36
Matt Billenstein <matt@vazor.com>
2023-03-02 22:51:10 -0800

    Cleanup 2022 Day 1

---
+++ 2022/01/p.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pypy3
+
+import sys
+from collections import defaultdict
+
+def parse_input():
+    lines = [_.strip('\r\n') for _ in sys.stdin]
+    elves = defaultdict(int)
+    i = 0
+    for line in lines:
+        if not line:
+            i += 1
+            continue
+        elves[i] += int(line)
+    return elves
+
+def part(elves):
+    elves = list(elves.items())
+    elves.sort(key=lambda x: x[1])
+
+    # part 1
+    print(len(elves), elves[-1])
+
+    # part 2, sum of top 3
+    print(elves[-3:])
+    print(sum(_[1] for _ in elves[-3:]))
+
+def main():
+    data = parse_input()
+    part(data)
+
+if __name__ == '__main__':
+    main()
