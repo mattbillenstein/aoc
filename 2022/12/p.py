6e32d6f7be28b33fe4ab272ee44e95f7a9830277
Matt Billenstein <matt@vazor.com>
2023-03-03 14:33:43 -0800

    Rework 2022 Day 12

--- 2022/12/p.py
+++ 2022/12/p.py
@@ -1,156 +1,53 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 
 import sys
-import time
-from collections import namedtuple
 
-INF = sys.maxsize
+from graph import bfs
+from grid import Grid
 
-letters = 'SabcdefghijklmnopqrstuvwxyzE'
+DEBUG = sys.argv.count('-v')
 
-START = letters.index('S')
-END = letters.index('E')
+def debug(*args):
+    if DEBUG:
+        print(*args)
 
-Point = namedtuple("Point", "x y")
+def parse_input():
+    lines = [_.strip('\r\n') for _ in sys.stdin]
+    return Grid(lines, {'S': ord('a')-1, 'E': ord('z')+1})
 
-class Node:
-    def __init__(self, x, y, h, dist=INF, prev=None):
-        self.x = x
-        self.y = y
-        self.h = h
-        self.dist = dist
-        self.prev = prev
+def part(grid):
+    if DEBUG:
+        grid.print()
 
-    def neighbors(self):
-        return [
-            (self.x+1, self.y),
-            (self.x-1, self.y),
-            (self.x,   self.y+1),
-            (self.x,   self.y-1),
-        ]
+    As = []
+    for pt in grid:
+        c = grid.getc(pt)
+        if c == 'S':
+            start = pt
+        elif c == 'E':
+            end = pt
+        elif c == 'a':
+            As.append(pt)
 
-    def __repr__(self):
-        return f'Node({self.x}, {self.y}, {self.h}, {self.dist})'
+    def neighbors(pt):
+        v = grid.get(pt)
+        for npt in grid.neighbors4(pt):
+            nv = grid.get(npt)
+            if nv - v <= 1:
+                yield npt
 
+    # part1 min dist S -> E
+    dist = bfs(start, neighbors, end)
+    print(dist)
 
-def print_grid(grid):
-    for row in grid:
-        print(''.join(letters[_] for _ in row))
+    # part2 min dist E -> any 'a'
+    dists = [bfs(_, neighbors, end) for _ in As]
+    dist = min(_ for _ in dists if _ is not None)
+    print(dist)
 
-
-def print_path(node, sizex, sizey):
-    grid = [['.'] * sizex for _ in range(sizey)]
-    prev = None
-    cnt = 0
-    while node:
-        cnt += 1
-        c = '*'
-        if node.h == END:
-            c = 'E'
-        elif not node.prev:
-            c = 'S'
-        elif prev:
-            if node.x < prev.x:
-                c = '>'
-            elif node.x > prev.x:
-                c = '<'
-            elif node.y > prev.y:
-                c = '^'
-            elif node.y < prev.y:
-                c = 'v'
-
-        grid[node.y][node.x] = c
-
-        prev = node
-        node = node.prev
-
-    print(cnt-1)
-    for row in grid:
-        print(''.join(row))
-
-
-def search(grid, startpt, endpt):
-    # djikstra's algorithm
-    visited = {}
-    unvisited = {}
-
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            unvisited[(x, y)] = Node(x, y, grid[y][x])
-
-    # set start point dist to 0
-    unvisited[(startpt.x, startpt.y)].dist = 0
-    end_node = unvisited[(endpt.x, endpt.y)]
-
-    while unvisited:
-        pt = None
-        dist = INF
-        for k, n in unvisited.items():
-            if n.dist <= dist:
-                pt = k
-                dist = n.dist
-
-        node = unvisited.pop(pt)
-
-        for x, y in node.neighbors():
-            other = unvisited.get((x, y))
-            if not other or (other.h - node.h) > 1:
-                continue
-
-            alt_dist = node.dist + 1
-            if alt_dist < other.dist:
-                other.dist = alt_dist
-                other.prev = node
-
-                if other is end_node:
-                    return other
-
-
-def main(argv):
-    with open(argv[1]) as f:
-        lines = [_.strip('\r\n') for _ in f]
-
-    grid = []
-    for y, line in enumerate(lines):
-        row = [letters.index(c) for c in line]
-        grid.append(row)
-        if START in row:
-            startpt = Point(row.index(START), y)
-        if END in row:
-            endpt = Point(row.index(END), y)
-
-    gridx = len(grid[0])
-    gridy = len(grid)
-
-    print(startpt, endpt)
-
-    print_grid(grid)
-
-    # part 1, min distance from start to end...
-    node = search(grid, startpt, endpt)
-
-    print()
-    print(node)
-    print_path(node, gridx, gridy)
-
-    # part 2, consider all starting points elevation 'a' and 'S' as start, what
-    # is the min dist from any of these points to end
-    min_dist = INF
-    for y, row in enumerate(grid):
-        for x, c in enumerate(row):
-            if c <= letters.index('a'):
-                node = search(grid, Point(x, y), endpt)
-                if node and node.dist < min_dist:
-                    min_dist = node.dist
-
-                if node:
-                    print()
-                    print(node)
-                    print_path(node, gridx, gridy)
-
-    print()
-    print(min_dist)
-
+def main():
+    data = parse_input()
+    part(data)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
