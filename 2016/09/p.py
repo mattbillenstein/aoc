a5ff4e025f37075110d0e112e4159c95f1170a4c
Matt Billenstein <matt@vazor.com>
2023-03-04 00:15:54 -0800

    Speed up 2016 Day 9 - expand each expansion once, not N times

--- 2016/09/p.py
+++ 2016/09/p.py
@@ -10,46 +10,51 @@ def debug(*args):
 
 def parse_input():
     lines = [_.strip('\r\n') for _ in sys.stdin]
-    return lines
+    return lines[0]
 
-def part1(data):
-    for line in data:
-        debug(line)
-        pos = 0
-        while 1:
-            i = line.find('(', pos)
-            if i == -1:
-                break
-            j = line.index(')', i)
-            a, b = line[i+1:j].split('x')
-            a = int(a)
-            b = int(b)
-            s = line[j+1:j+1+a]
-            line = line[:i] + (s * b) + line[j+1+a:]
-            pos = i + len(s) * b
+def part1(line):
+    debug(line)
+    pos = 0
+    while 1:
+        i = line.find('(', pos)
+        if i == -1:
+            break
+        j = line.index(')', i)
+        a, b = line[i+1:j].split('x')
+        a = int(a)
+        b = int(b)
+        s = line[j+1:j+1+a]
+        line = line[:i] + (s * b) + line[j+1+a:]
+        pos = i + len(s) * b
 
-        debug(line)
-        print(len(line))
+    debug(line)
+    print(len(line))
 
-def part2(data):
-    for line in data:
-        cnt = 0
-        debug(line)
-        while 1:
-            i = line.find('(')
-            if i == -1:
-                cnt += len(line)
-                break
-            cnt += i
+def part2(line):
+    L = [(line, 1)]
 
-            j = line.index(')', i)
-            a, b = line[i+1:j].split('x')
-            a = int(a)
-            b = int(b)
-            s = line[j+1:j+1+a]
-            line = (s * b) + line[j+1+a:]
+    cnt = 0
+    debug(line)
+    while L:
+        line, repeat = L.pop()
+        i = line.find('(')
+        if i == -1:
+            cnt += len(line) * repeat
+            continue
 
-        print(cnt)
+        cnt += i * repeat
+
+        j = line.index(')', i)
+        a, b = line[i+1:j].split('x')
+        a = int(a)
+        L.append((line[j+1+a:], repeat))
+
+        b = int(b)
+        s = line[j+1:j+1+a]
+        if s:
+            L.append((s, b * repeat))
+
+    print(cnt)
 
 def main():
     data = parse_input()
