4ca2f0287cb61a72e7ccfe69181edc7782aaa0e8
Matt Billenstein <matt@vazor.com>
2023-03-04 03:56:36 -0800

    Answers script

--- get-input.sh
+++ get-input.sh
@@ -7,7 +7,7 @@ COOKIE="$(cat ~/src/mattb/dotfiles/aoc)"
 YEAR="$(basename $(dirname $PWD))"
 DAY="$(basename $PWD | sed -e 's/^0//')"
 
-UA="github.com/mattbillenstein/aoc/get-data.sh v0 by matt@vazor.com"
+UA="github.com/mattbillenstein/aoc/get-input.sh v0 by matt@vazor.com"
 curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$DAY/input > input.txt
 
 if [ ! -e p.py ]; then
