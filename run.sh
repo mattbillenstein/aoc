63211ef734efc1f07bed67b83ce98b7ea00ced25
Matt Billenstein <matt@vazor.com>
2023-03-06 17:40:42 -0800

    Allow overriding interpreter

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ START=$(date +'%s')
 cd $1 > /dev/null
 
 if [ -e input-mod.txt ]; then
-  ./p.py 1 < input.txt > output.txt
-  ./p.py 2 < input-mod.txt >> output.txt
+  $AOCPY ./p.py 1 < input.txt > output.txt
+  $AOCPY ./p.py 2 < input-mod.txt >> output.txt
 else
-  ./p.py 1 2 < input.txt > output.txt
+  $AOCPY ./p.py 1 2 < input.txt > output.txt
 fi
 
 END=$(date +'%s')
