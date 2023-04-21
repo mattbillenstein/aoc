5f7b0b44d3e4eec61ce0f7e2ce52af674df51540
Matt Billenstein <matt@vazor.com>
2023-04-21 15:25:56 -0500

    Simpler

--- 2016/05/p.py
+++ 2016/05/p.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
-import hashlib
 import sys
-
-DEBUG = sys.argv.count('-v')
-
-def debug(*args):
-    if DEBUG:
-        print(*args)
+from hashlib import md5
 
 def parse_input():
     lines = [_.strip('\r\n') for _ in sys.stdin]
@@ -18,15 +12,14 @@ def part(data):
     t = [''] * 8
     i = 0
     while len(s) < 8 or not all(t):
-        b = (data + str(i)).encode('utf8')
-        h = hashlib.md5(b).hexdigest()
+        h = md5((data + str(i)).encode()).hexdigest()
         if h[:5] == '00000':
             s += h[5]
-            debug(b, h, s, t)
             pos = int(h[5], 16)
             if pos < len(t) and not t[pos]:
                 t[pos] = h[6]
         i += 1
+
     print(s[:8])
     print(''.join(t[:8]))
 
