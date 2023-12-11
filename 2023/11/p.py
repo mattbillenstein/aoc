539b693790ac9b78fd387444caf4114cbd026d04
Matt Billenstein <matt@vazor.com>
2023-12-10 23:43:14 -0800

    Correct way to do this...

--- 2023/11/p.py
+++ 2023/11/p.py
@@ -1,67 +1,47 @@
 #!/usr/bin/env pypy3
 
-import copy
 import itertools
-import math
 import sys
-import time
-from collections import defaultdict
-from pprint import pprint
-
-from grid import SparseGrid
-
-DEBUG = sys.argv.count('-v')
-
-def debug(*args):
-    if DEBUG:
-        print(*args)
 
 def parse_input():
     lines = [_.strip('\r\n') for _ in sys.stdin]
-    return SparseGrid(lines)
+    s = set()
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == '#':
+                s.add((x, y))
+    return s
 
 def manhattan(a, b):
     return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
-def part1(grid, N=2):
-    # expand
-    ngrid = grid.copy()
+def part1(pts, N=2):
+    npts = set(pts)
 
-    sx, sy = ngrid.size
-
-    for y in range(sy-1, -1, -1):
-        row = [pt for pt, _ in ngrid.sparse_iter() if pt[1] == y and ngrid.getc(pt) == '#']
+    for y in range(max(_[1] for _ in npts), -1, -1):
+        row = [_ for _ in npts if _[1] == y]
         if not row:
-            pts = [pt for pt, _ in ngrid.sparse_iter() if pt[1] > y and ngrid.getc(pt) == '#']
-            pts.sort(key=lambda pt: (pt[1], pt[0]), reverse=True)
-            for pt in pts:
+            L = [_ for _ in npts if _[1] > y]
+            L.sort(key=lambda pt: (pt[1], pt[0]), reverse=True)
+            for pt in L:
                 npt = (pt[0], pt[1] + (N-1))
-                ngrid.setc(npt, '#')
-                ngrid.setc(pt, '.')
-                    
-    sx, sy = ngrid.size
+                npts.add(npt)
+                npts.remove(pt)
 
-    for x in range(sx-1, -1, -1):
-        col = [pt for pt, _ in ngrid.sparse_iter() if pt[0] == x and ngrid.getc(pt) == '#']
+    for x in range(max(_[0] for _ in npts), -1, -1):
+        col = [_ for _ in npts if _[0] == x]
         if not col:
-            pts = [pt for pt, _ in ngrid.sparse_iter() if pt[0] > x and ngrid.getc(pt) == '#']
-            pts.sort(reverse=True)
-            for pt in pts:
+            L = [_ for _ in npts if _[0] > x]
+            L.sort(reverse=True)
+            for pt in L:
                 npt = (pt[0] + (N-1), pt[1])
-                ngrid.setc(npt, '#')
-                ngrid.setc(pt, '.')
-
-    pts = [pt for pt, _ in ngrid.sparse_iter() if ngrid.getc(pt) == '#']
+                npts.add(npt)
+                npts.remove(pt)
 
     tot = 0
-    for a, b in itertools.combinations(pts, 2):
+    for a, b in itertools.combinations(npts, 2):
         tot += manhattan(a, b)
     print(tot)
-
-    if DEBUG:
-        grid.print()
-        print()
-        ngrid.print()
 
 def part2(data):
     part1(data, 1000000)
