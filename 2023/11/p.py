788b78b4e756841a1101259efd0c28eab28b9c68
Matt Billenstein <matt@vazor.com>
2023-12-10 23:51:08 -0800

    More simpler

--- 2023/11/p.py
+++ 2023/11/p.py
@@ -5,53 +5,47 @@ import sys
 
 def parse_input():
     lines = [_.strip('\r\n') for _ in sys.stdin]
-    s = set()
+    pts = set()
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
             if c == '#':
-                s.add((x, y))
-    return s
+                pts.add((x, y))
+    return pts
 
 def manhattan(a, b):
     return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
 def part1(pts, N=2):
-    npts = set(pts)
-
-    for y in range(max(_[1] for _ in npts), -1, -1):
-        row = [_ for _ in npts if _[1] == y]
+    for y in range(max(_[1] for _ in pts), -1, -1):
+        row = [_ for _ in pts if _[1] == y]
         if not row:
-            L = [_ for _ in npts if _[1] > y]
+            L = [_ for _ in pts if _[1] > y]
             L.sort(key=lambda pt: (pt[1], pt[0]), reverse=True)
             for pt in L:
-                npt = (pt[0], pt[1] + (N-1))
-                npts.add(npt)
-                npts.remove(pt)
+                pts.add((pt[0], pt[1] + (N-1)))
+                pts.remove(pt)
 
-    for x in range(max(_[0] for _ in npts), -1, -1):
-        col = [_ for _ in npts if _[0] == x]
+    for x in range(max(_[0] for _ in pts), -1, -1):
+        col = [_ for _ in pts if _[0] == x]
         if not col:
-            L = [_ for _ in npts if _[0] > x]
+            L = [_ for _ in pts if _[0] > x]
             L.sort(reverse=True)
             for pt in L:
-                npt = (pt[0] + (N-1), pt[1])
-                npts.add(npt)
-                npts.remove(pt)
+                pts.add((pt[0] + (N-1), pt[1]))
+                pts.remove(pt)
 
-    tot = 0
-    for a, b in itertools.combinations(npts, 2):
-        tot += manhattan(a, b)
+    tot = sum(manhattan(a, b) for a, b in itertools.combinations(pts, 2))
     print(tot)
 
-def part2(data):
-    part1(data, 1000000)
+def part2(pts):
+    part1(pts, 1000000)
 
 def main():
     data = parse_input()
     if '1' in sys.argv:
-        part1(data.copy())
+        part1(set(data))
     if '2' in sys.argv:
-        part2(data.copy())
+        part2(set(data))
 
 if __name__ == '__main__':
     main()
