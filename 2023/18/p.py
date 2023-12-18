b01acbc21dd68031a0e62a706da07241016cc60a
Matt Billenstein <matt@vazor.com>
2023-12-18 04:12:29 -0800

    2023 Day 18

--- 2023/18/p.py
+++ 2023/18/p.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env pypy3
 
-import copy
-import itertools
-import math
 import sys
-import time
-from collections import defaultdict
-from pprint import pprint
 
 from graph import bfs
 from grid import SparseGrid
 
 DEBUG = sys.argv.count('-v')
-
-def debug(*args):
-    if DEBUG:
-        print(*args)
 
 def parse_input():
     lines = [_.strip('\r\n') for _ in sys.stdin]
@@ -27,86 +17,14 @@ def parse_input():
         directions.append([dir, dist, color])
     return directions
 
-def is_corner(edge, y):
-    return edge[0][1] == y or edge[1][1] == y
-
-def both_corners(edge1, edge2, y):
-    return is_corner(edge1, y) and is_corner(edge2, y)
-
-def corners_same_dir(edge1, edge2, y):
-    return both_corners(edge1, edge2, y) and \
-        ((edge1[0][1] < y and edge2[0][1] < y) or (edge1[1][1] > y and edge2[1][1] > y))
-
-def fill(edges):
-    # for every y, fill left to right
-    area = 0
-    mn = sys.maxsize
-    mx = 0
-    for pts in edges:
-        for pt in pts:
-            if pt[1] < mn:
-                mn = pt[1]
-            if pt[1] > mx:
-                mx = pt[1]
-
-    for y in range(mn, mx+1):
-        # find edges that overlap
-        L = [_ for _ in edges if _[0][1] <= y <= _[1][1]]
-
-        # sort by x
-        L.sort()
-
-        inside = False
-        for i in range(len(L)-1):
-            filled = False
-            edge1 = L[i]
-            edge2 = L[i+1]
-
-            if corners_same_dir(edge1, edge2, y):
-                # fill horizontal edge, still either in or out
-                area += edge2[0][0] - edge1[0][0]
-                if not inside:
-                    area += 1
-                filled = True
-                print(y, 'fill corners same dir', edge1[0][0], edge2[0][0], area, inside)
-            elif both_corners(edge1, edge2, y):
-                # fill between corners, if we were outside, now in, if in, still in...
-                area += edge2[0][0] - edge1[0][0]
-                filled = True
-                if not inside:
-                    inside = True
-                print(y, 'fill both corners', edge1[0][0], edge2[0][0], area, inside)
-            elif not is_corner(edge1, y) and not is_corner(edge2, y):
-                # standard case, fill of we're transitioning out -> in
-                inside = not inside
-                if inside:
-                    area += edge2[0][0] - edge1[0][0] + 1
-                    filled = True
-                    print(y, 'fill regular', edge1[0][0], edge2[0][0], area, inside)
-            else:
-                # came to an edge. fill and flip
-                if inside:
-                    area += edge2[0][0] - edge1[0][0] + 1
-                    filled = True
-                    print(y, 'fill normal', edge1[0][0], edge2[0][0], area, inside)
-                else:
-                    print(y, 'fill skip', edge1[0][0], edge2[0][0], area, inside)
-                
-                inside = not inside
-
-        print(y, L, area)
-
-    return area
-
 def part1(directions):
+    # build grid and bfs; count set cells
     g = SparseGrid({})
     pt = (0, 0)
     for dir, dist, _ in directions:
         for i in range(dist):
             g.set(pt, 1)
             pt = g.step(pt, dir)
-
-    g.print()
 
     for y in g.ys:
         for x in g.xs:
@@ -131,37 +49,77 @@ def part1(directions):
 
     print(cnt)
 
-def part1a(data):
-    pt = (0, 0)
-    edges = []
-    for dir, dist, _ in data:
-        if dir == 'U':
-            npt = (pt[0], pt[1] - dist)
-        elif dir == 'D':
-            npt = (pt[0], pt[1] + dist)
-        elif dir == 'R':
-            npt = (pt[0] + dist, pt[1])
-        elif dir == 'L':
-            npt = (pt[0] - dist, pt[1])
-        else:
-            assert 0
+def fill(edges):
+    # scanned left to right, vertical edges pointing up followed by edges
+    # pointing down are inside assuming a clockwise path?
+    #
+    # Scan each Y left to right and take the area of such lines; then we
+    # just need to include horizontal lines cononecting Y's in other
+    # directions...
+    #
+    # This reduction could be repeated in X as well, repeating computed area as
+    # long as the vertical edges are the same.
 
-        if dir in 'UD':
-            if pt[1] <= npt[1]:
-                edges.append((pt, npt))
-            else:
-                edges.append((npt, pt))
+    vedges = [_ for _ in edges if _.pt1[0] == _.pt2[0]]
+    hedges = [_ for _ in edges if _.pt1[1] == _.pt2[1]]
 
-        pt = npt
+    connectors = set([(_.pt1, _.pt2) for _ in hedges])
 
-    print(fill(edges))
+    area = 0
+    mn, mx = sys.maxsize, 0
+    for edge in vedges:
+        for pt in edge:
+            if pt[1] < mn:
+                mn = pt[1]
+            if pt[1] > mx:
+                mx = pt[1]
+
+    for y in range(mn, mx+1):
+        # find edges that overlap
+        L = [_ for _ in vedges if _.pt1[1] <= y <= _.pt2[1]]
+
+        # sort by x
+        L.sort(key=lambda e: e.pt1[0])
+
+        counted = -10
+        for i in range(len(L)-1):
+            edge1 = L[i]
+            edge2 = L[i+1]
+
+            x1 = edge1.pt1[0]
+            x2 = edge2.pt1[0]
+
+            if edge1.dir == 'U' and edge2.dir == 'D':
+                # normal area fill
+                area += x2 - x1 + (0 if counted == i-1 else 1)
+                counted = i
+            elif ((x1, y), (x2, y)) in connectors:
+                area += x2 - x1 + (0 if counted == i-1 else 1)
+                counted = i
+
+    return area
+
+class Edge:
+    def __init__(self, pt1, pt2, dir):
+        self.pt1 = pt1
+        self.pt2 = pt2
+        self.dir = dir
+
+    def __repr__(self):
+        return f"Edge({self.pt1[0]}, {self.pt1[1]}-{self.pt2[1]}, {self.dir})"
+
+    def __iter__(self):
+        yield self.pt1
+        yield self.pt2
 
 def part2(data):
     pt = (0, 0)
     edges = []
-    for _, _, color in data:
-        dir = {'0': 'R', '1': 'D', '2': 'L', '3': 'U'}[color[-1]]
-        dist = int(color[:-1], 16)
+    for dir, dist, color in data:
+        # if debug, don't unpack the actual thing... Use the given input.
+        if DEBUG == 0:
+            dir = {'0': 'R', '1': 'D', '2': 'L', '3': 'U'}[color[-1]]
+            dist = int(color[:-1], 16)
 
         if dir == 'U':
             npt = (pt[0], pt[1] - dist)
@@ -174,21 +132,27 @@ def part2(data):
         else:
             assert 0
 
-        # don't need horizontal edges, just vertical...
         if dir in 'UD':
-            edges.append((pt, npt))
+            # order so lesser y coordinate is pt1
+            if pt[1] <= npt[1]:
+                edge = Edge(pt, npt, dir)
+            else:
+                edge = Edge(npt, pt, dir)
+        else:
+            if pt[0] <= npt[0]:
+                edge = Edge(pt, npt, dir)
+            else:
+                edge = Edge(npt, pt, dir)
+
+        edges.append(edge)
 
         pt = npt
-
-#    print(edges)
     print(fill(edges))
         
 def main():
     data = parse_input()
     if '1' in sys.argv:
         part1(data)
-    if '1a' in sys.argv:
-        part1a(data)
     if '2' in sys.argv:
         part2(data)
 
