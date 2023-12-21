17ae0be63553d1715d1c78ba8b0435cf8d1e1bac
Matt Billenstein <matt@vazor.com>
2023-12-21 05:09:31 -0800

    2023 Day 21 working...

--- 2023/21/p.py
+++ 2023/21/p.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env pypy3
 
-import copy
-import itertools
-import math
 import sys
-import time
-from collections import defaultdict
 from pprint import pprint
 
 from graph import bfs
@@ -52,6 +47,7 @@ def fill(grid, start, mdist=64, parity=0):
     return pts
 
 def part1(grid, n=64):
+    grid = grid.copy()
     for pt in grid:
         c = grid.getc(pt)
         if c == 'S':
@@ -96,6 +92,9 @@ def part2(grid):
     # here as parity since the fill alternates on even/odd coordinates given
     # the tile size is odd...
 
+    # FIXME - so figured out finally that N is odd, so we're only interested in
+    # parity = 1 with this copy/paste strategy...
+
     # allowed distance in part2
     N = 26501365
 
@@ -133,7 +132,7 @@ def part2(grid):
     assert (nstart[0] + ndist) % size == (start[0] + N) % size == 130
 
     # Fill from start in both parities
-    pts = [fill(ngrid.copy(), nstart, ndist, _) for _ in range(2)]
+    pts = [fill(ngrid.copy(), nstart, ndist, _) for _ in (0, 1)]
 
     # Extract tiles for each position and parity...
     tiles = {}
@@ -144,27 +143,42 @@ def part2(grid):
                 y0, y1 = y * size, y * size + size
                 tiles[(x, y, p)] = sum(1 for _ in pts[p] if x0 <= _[0] < x1 and y0 <= _[1]  < y1)
 
-    pprint(tiles)
+    if DEBUG:
+        pprint(tiles)
+
+    # small ul
+    assert tiles[(1, 0, 0)] == tiles[(0, 1, 0)]
+    # small ur
+    assert tiles[(3, 0, 0)] == tiles[(4, 1, 0)]
+    # small ll
+    assert tiles[(0, 3, 0)] == tiles[(1, 4, 0)]
+    # small lr
+    assert tiles[(3, 4, 0)] == tiles[(4, 3, 0)]
 
     partial_small_ul = tiles[(1, 0, 1)]
-    top_center = tiles[(2, 0, 0)]
+    top_center = tiles[(2, 0, 1)]
     partial_small_ur = tiles[(3, 0, 1)]
 
-    partial_big_ul = tiles[(1, 1, 0)]
-    partial_big_ur = tiles[(3, 1, 0)]
+    partial_big_ul = tiles[(1, 1, 1)]
+    partial_big_ur = tiles[(3, 1, 1)]
 
-    full_odd = tiles[(2, 1, 1)]
-    full_even = tiles[(2, 2, 0)]
+    full_even = tiles[(2, 2, 1)]
+    full_odd = tiles[(2, 1, 1)]   # 2, 1, 1 is double parity...
 
-    partial_big_ll = tiles[(1, 3, 0)]
-    partial_big_lr = tiles[(3, 3, 0)]
+    partial_big_ll = tiles[(1, 3, 1)]
+    partial_big_lr = tiles[(3, 3, 1)]
 
     partial_small_ll = tiles[(1, 4, 1)]
-    bottom_center = tiles[(2, 4, 0)]
+    bottom_center = tiles[(2, 4, 1)]
     partial_small_lr = tiles[(3, 4, 1)]
 
-    left_center = tiles[(0, 2, 0)]
-    right_center = tiles[(4, 2, 0)]
+    left_center = tiles[(0, 2, 1)]
+    right_center = tiles[(4, 2, 1)]
+
+    assert partial_small_ll < partial_big_ll
+    assert partial_small_lr < partial_big_lr
+    assert partial_small_ul < partial_big_ul
+    assert partial_small_ur < partial_big_ur
 
     # add them up
     cnt = 0
@@ -184,13 +198,14 @@ def part2(grid):
         cnt += partial_small_ul + partial_big_ul + partial_big_ur + partial_small_ur
         cnt += odd * full_odd
         cnt += even * full_even
-        print(i, odd, even, (odd + even) * size, N*2)
+        debug(i, odd, even, (odd + even) * size, N*2)
 
         even += 2
         odd, even = even, odd
 
     # center row
     cnt += left_center + odd * full_odd + even * full_even + right_center
+    debug('center', odd, even, (odd + even) * size, N*2)
     rows += 1
 
     assert size + odd * size + even * size + size - 1 == N*2
@@ -202,31 +217,30 @@ def part2(grid):
         odd, even = even, odd
         even -= 2
 
-        cnt += partial_small_ul + partial_big_ul + partial_big_ur + partial_small_ur
+        cnt += partial_small_ll + partial_big_ll + partial_big_lr + partial_small_lr
         cnt += odd * full_odd
         cnt += even * full_even
-        print(i, odd, even, (odd + even) * size, N*2)
-
+        debug(i, odd, even, (odd + even) * size, N*2)
 
     # bottom row
-    cnt += partial_small_ul + top_center + partial_small_ur
+    cnt += partial_small_ll + bottom_center + partial_small_lr
     rows += 1
 
-    print(cnt, rows, rows * size, N*2)
+    debug(cnt, rows, rows * size - 1, N*2)
 
-    assert cnt < 609789877775394 # too high
-    assert cnt < 609786863497944
+    assert cnt > 600786863497944 # too low
+    assert cnt < 609786863497944 # too high
 
     print(cnt)
 
 def main():
     data = parse_input()
     if '1' in sys.argv:
-        part1(copy.deepcopy(data))
+        part1(data)
     if '2' in sys.argv:
-        part2(copy.deepcopy(data))
+        part2(data)
     if '2a' in sys.argv:
-        part2a(copy.deepcopy(data))
+        part2a(data)
 
 if __name__ == '__main__':
     main()
