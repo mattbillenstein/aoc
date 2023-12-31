3bed0262c808bb653cf25ddeec300f80d42dc3d8
Matt Billenstein <matt@vazor.com>
2023-12-31 15:18:50 -0800

    Amend get-input.sh for aoc-input

--- get-input.sh
+++ get-input.sh
@@ -7,8 +7,12 @@ COOKIE="$(cat ~/src/mattb/dotfiles/aoc)"
 YEAR="$(basename $(dirname $PWD))"
 DAY="$(basename $PWD | sed -e 's/^0//')"
 
+mkdir -p ../../../aoc-input/$YEAR/$DAY
+
 UA="github.com/mattbillenstein/aoc/get-input.sh v0 by matt@vazor.com"
-curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$DAY/input > input.txt
+curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$DAY/input > ../../../aoc-input/$YEAR/$DAY/input.txt
+
+ln -s ../../../aoc-input/$YEAR/$DAY/input.txt .
 
 if [ ! -e p.py ]; then
   cp ../../p.py .
