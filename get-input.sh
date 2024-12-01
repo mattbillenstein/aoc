f2d1e1e44809c5ba693e12bb47950c6a3c3506a4
Matt Billenstein <matt@vazor.com>
2024-11-30 21:22:21 -0800

    Fixup get-input.sh day linking

--- get-input.sh
+++ get-input.sh
@@ -5,12 +5,12 @@ set -eo pipefail
 COOKIE="$(cat ~/src/mattb/dotfiles/aoc)"
 
 YEAR="$(basename $(dirname $PWD))"
-DAY="$(basename $PWD | sed -e 's/^0//')"
+DAY="$(basename $PWD)"
 
 mkdir -p ../../../aoc-input/$YEAR/$DAY
 
 UA="github.com/mattbillenstein/aoc/get-input.sh v0 by matt@vazor.com"
-curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$DAY/input > ../../../aoc-input/$YEAR/$DAY/input.txt
+curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$(echo $DAY | sed -e 's/^0//')/input > ../../../aoc-input/$YEAR/$DAY/input.txt
 
 ln -s ../../../aoc-input/$YEAR/$DAY/input.txt .
 
