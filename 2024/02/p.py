a82b19db37f6e913a8251fa7d5a0442050d57e7b
Matt Billenstein <matt@vazor.com>
2024-12-01 21:25:22 -0800

    Day 2 cleanup

--- 2024/02/p.py
+++ 2024/02/p.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env pypy3
 
-import copy
-import itertools
-import math
 import sys
-import time
-from collections import defaultdict
-from pprint import pprint
 
 DEBUG = sys.argv.count('-v')
 
@@ -19,34 +13,35 @@ def parse_input():
     L = [[int(x) for x in _.split()] for _ in lines]
     return L
 
+def check_safe(L):
+    return all(1 <= abs(_) <= 3 for _ in L) and len(set(_//abs(_) for _ in L)) == 1
+
 def part1(data):
     safe = 0
     for L in data:
         L2 = [L[i]-L[i+1] for i in range(len(L)-1)]
-        if all(1 <= abs(_) <= 3 for _ in L2) and len(set(_//abs(_) for _ in L2)) == 1:
+        if check_safe(L2):
             safe += 1
     print(safe)
 
 def part2(data):
     safe = 0
     for L in data:
-        is_safe = False
+        # iterate over all lists removing one item and check
         for i in range(len(L)):
             L3 = [x for j, x in enumerate(L) if j != i]
             L2 = [L3[i]-L3[i+1] for i in range(len(L3)-1)]
-            if all(1 <= abs(_) <= 3 for _ in L2) and len(set(_//abs(_) for _ in L2)) == 1:
-                is_safe = True
+            if check_safe(L2):
+                safe += 1
                 break
-        if is_safe:
-            safe += 1
     print(safe)
 
 def main():
     data = parse_input()
     if '1' in sys.argv:
-        part1(copy.deepcopy(data))
+        part1(data)
     if '2' in sys.argv:
-        part2(copy.deepcopy(data))
+        part2(data)
 
 if __name__ == '__main__':
     main()
