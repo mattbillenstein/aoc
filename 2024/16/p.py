f017475153962b05bcbb4d765970be163d3d2e35
Matt Billenstein <matt@vazor.com>
2024-12-16 15:46:01 -0800

    Working scheme for part 1, setup for part 2

--- 2024/16/p.py
+++ 2024/16/p.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env pypy3
 
 import sys
+from collections import defaultdict
 from pprint import pprint
 
 from grid import Grid
-from graph import dijkstra
+from graph import dfs, PriorityQueue
 
 DEBUG = sys.argv.count('-v')
 
@@ -17,134 +18,93 @@ def parse_input():
     g = Grid(lines, {c: i for i, c in enumerate('.#<^>vSEo')})
     return (g,)
 
-def trace(grid, pt, dir, vertices, visited):
-    # trace to next junction, return junction and distance
-    # from 2023/23
-    visited.append(pt)
-    dist = 1
-    while 1:
-        npt = grid.step(pt, dir)
-        c = grid.getc(npt)
-        if c in ('.', dir):
-            pt = npt
-            dist += 1
-            visited.append(pt)
-        elif c == '#':
-            found = False
-            for ndir in {'^': '<>', 'v': '<>', '<': '^v', '>': '^v'}[dir]:
-                npt = grid.step(pt, ndir)
-                if npt and npt not in visited and grid.getc(npt) in ('.', ndir):
-                    pt = npt
-                    dir = ndir
-                    dist += 1
-                    dist += 1000  # direct turn in path
-                    visited.append(pt)
-                    found = True
-                    break
 
-            if not found:
-                return None
-        else:
-            assert c in '<>v^'
-            return None
+# modified dijkstra to return a set of parent nodes instead of only one
+def dijkstra(graph, start):
+    pq = PriorityQueue()
+    dist = {}
+    prev = {}
+    for v in graph:
+        dist[v] = d = 0 if v == start else sys.maxsize
+        prev[v] = set()
+        pq.add_task(v, d)
 
-        if pt in vertices:
-            return (pt, dir, dist, visited)
+    while pq:
+        u = pq.pop_task()
+        for v, w in graph[u]:
+            if v in pq:
+                alt = dist[u] + w
+                if alt <= dist[v]:
+                    pq.add_task(v, alt)
+                    dist[v] = alt
+                    prev[v].add((u, alt))
 
-    assert 0
+    return dist, prev
 
-def part1(g):
-    g = g.copy()
+def part(g):
     if DEBUG:
         g.print()
 
     # directions and their opposite direction
     dirs = {'<': '>', '>': '<', '^': 'v', 'v': '^'}
+    # turn directions
+    tdirs = {'>': '^v', '^': '<>', 'v': '<>', '<': '^v'}
 
-    # find start / end and clear
+    # find start/end and clear
     for pt in g:
         c = g.getc(pt)
-        if c == 'S':
-            start = pt
-            g.setc(pt, '.')
-        elif c == 'E':
-            end = pt
+        if c in 'SE':
+            if c == 'S':
+                start = pt
+            else:
+                end = pt
             g.setc(pt, '.')
 
-    # compress the grid into a graph, find vertices where we can turn first
-    vertices = {start: [], end: []}
+    # collect graaph edges for each '.'
+    graph = defaultdict(list)
     for pt in g:
-        if g.getc(pt) != '.':
+        c = g.getc(pt)
+        if c != '.':
             continue
-        N = sum(1 for _ in g.neighbors4(pt) if g.getc(_) == '.')
-        if N > 2:
-            vertices[pt] = []
 
-    graph = {}
-
-    # From each vertex, trace in each direction to the next vertex
-    for pt, L in vertices.items():
-        if pt == end:
-            # don't trace from end, we only go to it
-            continue
-        for dir in dirs:
+        for dir, odir in dirs.items():
+            # step off the tile in any same direction, cost 1
             npt = g.step(pt, dir)
-            if g.getc(npt) != '.':
-                continue
-            x = trace(g, npt, dir, vertices, [pt])
-            if x and x[0] != start:  # start is not an end
-                npt, ndir, dist, path = x
 
-                if npt == end:
-                    # add end node to graph ending with this direction
-                    graph[(npt, ndir)] = []
+            if g.getc(npt) == '.':
+                # step in same dir, cost 1
+                graph[(pt, dir)].append(((npt, dir), 1))
 
-                # add src -> dst vertex mapping if the end direction is
-                # valid or we hit end
-                if g.getc(g.step(npt, ndir)) == '.' or npt == end:
-                    graph.setdefault((pt, dir), []).append(((npt, ndir), dist))
+            # turn, cost 1000
+            for tdir in tdirs[dir]:
+                if g.getc(g.step(pt, tdir)) == '.':
+                    # valid turn
+                    graph[(pt, dir)].append(((pt, tdir), 1000))
 
-                # if we're not at an end, add nodes ending at this vertex
-                # with a valid turn, dist+1000
-                if npt != end:
-                    ndir_opp = dirs[ndir]
-                    for turn_dir in dirs:
-                        if turn_dir in (ndir, ndir_opp) or g.getc(g.step(npt, turn_dir)) != '.':
-                            continue
-                        graph.setdefault((pt, dir), []).append(((npt, turn_dir), dist+1000))
+    # add end nodes for end
+    for dir, odir in dirs.items():
+        if g.getc(g.step(end, dir)) == '.':
+            graph[(end, odir)] = []
 
-    # add nodes from the start if our starting direction isn't available...
-    if (start, '>') not in graph:
-        for dir in '^v':
-            if (start, dir) in graph:
-                graph.setdefault((start, '>'), []).extend([(pt, dist+1000) for pt, dist in graph[(start, dir)]])
+    if DEBUG > 1:
+        for k, v in sorted(graph.items()):
+            print(k, v)
 
-    # some turns added above go into dead-ends, remove them, but keep nodes to
-    # end
-    for k, L in graph.items():
-        # dead ends
-#        for x in L:
-#            if x[0] not in graph:
-#                print('missing', k, x[0], L)
-
-        L[:] = [_ for _ in L if _[0] in graph or _[0][0] == end]
-
-    # Ok, actual dijkstra
     dist, prev = dijkstra(graph, (start, '>'))
 
+    # Part 1
     # min distance to end point, we can end up there from different directions
     # with different costs
-    print(min(v for k, v in dist.items() if k[0] == end))
+    if '1' in sys.argv:
+        print(min(v for k, v in dist.items() if k[0] == end))
 
-def part2(data):
-    pass
+    # Part 2, how many tiles are touched by all equal paths
+    # Modified dijkstra to return multiple parents for each tile, use dfs from
+    # end to start to trace all paths...
 
 def main():
     data = parse_input()
-    if '1' in sys.argv:
-        part1(*data)
-    if '2' in sys.argv:
-        part2(*data)
+    part(*data)
 
 if __name__ == '__main__':
     main()
