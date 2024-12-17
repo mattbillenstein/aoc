e5ffb07a543347fdd086847cc4d1495081f572d3
Matt Billenstein <matt@vazor.com>
2024-12-16 16:34:44 -0800

    2024 Day 16 part 2

--- 2024/16/p.py
+++ 2024/16/p.py
@@ -2,25 +2,19 @@
 
 import sys
 from collections import defaultdict
-from pprint import pprint
 
 from grid import Grid
-from graph import dfs, PriorityQueue
+from graph import PriorityQueue
 
 DEBUG = sys.argv.count('-v')
 
-def debug(*args):
-    if DEBUG:
-        print(*args)
-
 def parse_input():
     lines = [_.strip('\r\n') for _ in sys.stdin]
-    g = Grid(lines, {c: i for i, c in enumerate('.#<^>vSEo')})
+    g = Grid(lines, {c: i for i, c in enumerate('.#<^>vSEO')})
     return (g,)
 
-
-# modified dijkstra to return a set of parent nodes instead of only one
 def dijkstra(graph, start):
+    # modified dijkstra to return a set of parent nodes instead of only one
     pq = PriorityQueue()
     dist = {}
     prev = {}
@@ -37,7 +31,7 @@ def dijkstra(graph, start):
                 if alt <= dist[v]:
                     pq.add_task(v, alt)
                     dist[v] = alt
-                    prev[v].add((u, alt))
+                    prev[v].add(u)
 
     return dist, prev
 
@@ -60,7 +54,7 @@ def part(g):
                 end = pt
             g.setc(pt, '.')
 
-    # collect graaph edges for each '.'
+    # collect graph edges for each '.'
     graph = defaultdict(list)
     for pt in g:
         c = g.getc(pt)
@@ -70,7 +64,6 @@ def part(g):
         for dir, odir in dirs.items():
             # step off the tile in any same direction, cost 1
             npt = g.step(pt, dir)
-
             if g.getc(npt) == '.':
                 # step in same dir, cost 1
                 graph[(pt, dir)].append(((npt, dir), 1))
@@ -82,8 +75,10 @@ def part(g):
                     graph[(pt, dir)].append(((pt, tdir), 1000))
 
     # add end nodes for end
+    ends = []
     for dir, odir in dirs.items():
         if g.getc(g.step(end, dir)) == '.':
+            ends.append((end, odir))
             graph[(end, odir)] = []
 
     if DEBUG > 1:
@@ -95,12 +90,59 @@ def part(g):
     # Part 1
     # min distance to end point, we can end up there from different directions
     # with different costs
+    cost = min(v for k, v in dist.items() if k[0] == end)
     if '1' in sys.argv:
-        print(min(v for k, v in dist.items() if k[0] == end))
+        print(cost)
+
+    if '2' not in sys.argv:
+        return
 
     # Part 2, how many tiles are touched by all equal paths
     # Modified dijkstra to return multiple parents for each tile, use dfs from
     # end to start to trace all paths...
+
+    _visited = set()  # must be ordered
+    def dfs(pos):
+        # dfs tracing backwards end -> start, yields paths start -> end
+        _visited.add(pos)
+        if pos == (start, '>'):
+            L = list(_visited)
+            L.reverse()
+            yield L
+        else:
+            for v in prev[pos]:
+                if v not in _visited:
+                    for p in dfs(v):
+                        yield p
+        _visited.remove(pos)
+
+    # for each end, dfs to start, compute cost of path, for paths with min
+    # cost, add tiles
+    tiles = set()
+    for e in ends:  # found earlier
+        for path in dfs(e):
+            assert path[0] == (start, '>')
+
+            t = path[0][1]
+            turns = 0
+            for pt, dir in path[1:]:
+                if dir != t:
+                    turns += 1
+                    t = dir
+
+            path_cost = len(set(_[0] for _ in path if _[0] != start))
+            path_cost += turns * 1000
+            if path_cost == cost:
+                # matching path, add tiles
+                tiles.update([_[0] for _ in path])
+
+    if DEBUG:
+        for pt in tiles:
+            g.setc(pt, 'O')
+        g.print()
+
+    # Output # tiles
+    print(len(tiles))
 
 def main():
     data = parse_input()
