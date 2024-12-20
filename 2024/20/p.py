015665a192ea236e3c16091421c42778f133c5bc
Matt Billenstein <matt@vazor.com>
2024-12-20 00:39:52 -0800

    Faster, only do the N^2 loop once

--- 2024/20/p.py
+++ 2024/20/p.py
@@ -5,8 +5,6 @@ from collections import defaultdict
 
 from grid import Grid, manhattan_distance
 
-DEBUG = sys.argv.count('-v')
-
 def parse_input():
     lines = [_.strip('\r\n') for _ in sys.stdin]
     g = Grid(lines, {c: i for i, c in enumerate('.#SE')})
@@ -46,39 +44,32 @@ def part(g):
     if len(path) < 1000:
         save = 50
 
-    for p in sys.argv[1:]:
-        if p == '1':
-            cheat = 2
-        elif p == '2':
-            cheat = 20
-        else:
-            continue
+    dists = []
+    if '1' in sys.argv:
+        dists.append(2)
+    if '2' in sys.argv:
+        dists.append(20)
 
-        # for each point on path in front of us, if it's manhattan-distance is
-        # <= cheat steps away, count it...
-        cheats = defaultdict(int)
-        tot = 0
-        for i, pt in enumerate(path[:-1]):
-            for npt in path[i+cheat:]:
-                md = manhattan_distance(pt, npt)
+    if not dists:
+        return
+
+    # for each point on path in front of us, if it's manhattan-distance is
+    # <= cheat steps away, count it...
+    cheats = defaultdict(int)
+    tot = 0
+    for i, pt in enumerate(path[:-1]):
+        for npt in path[i+min(dists):]:
+            md = manhattan_distance(pt, npt)
+            for cheat in dists:
                 if md <= cheat:
                     idx = idxs[npt]
                     new_len = len(path) - (idx - i) + md
                     saved = len(path) - new_len
-                    if saved > 0:
-                        cheats[saved] += 1
-                        if saved >= save:
-                            tot += 1
+                    if saved >= save:
+                        cheats[cheat] += 1
 
-        if DEBUG:
-            for k, v in sorted(cheats.items()):
-                if p == '1' or k >= save:
-                    print(k, v)
-
-        print(tot)
-
-def part2(data):
-    pass
+    for k in sorted(cheats):
+        print(cheats[k])
 
 def main():
     data = parse_input()
