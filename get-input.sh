1055b5f86e2e0bd46194fdf164f6ee9b21e6898f
Matt Billenstein <matt@vazor.com>
2025-12-01 21:34:45 -0800

    get-input.sh, error out if "Please log in" in input

--- get-input.sh
+++ get-input.sh
@@ -7,19 +7,23 @@ COOKIE="$(cat ~/src/mattb/dotfiles/aoc)"
 YEAR="$(basename $(dirname $PWD))"
 DAY="$(basename $PWD)"
 
-mkdir -p ../../../aoc-input/$YEAR/$DAY
+pushd ../../../aoc-input
+mkdir -p $YEAR/$DAY
 
 UA="github.com/mattbillenstein/aoc/get-input.sh v0 by matt@vazor.com"
-curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$(echo $DAY | sed -e 's/^0//')/input > ../../../aoc-input/$YEAR/$DAY/input.txt
+curl -s --header "Cookie: $COOKIE" --header "User-Agent: $UA" https://adventofcode.com/$YEAR/day/$(echo $DAY | sed -e 's/^0//')/input > $YEAR/$DAY/input.txt
 
-ln -s ../../../aoc-input/$YEAR/$DAY/input.txt .
-
-if [ ! -e p.py ]; then
-  cp ../../p.py .
+if grep -q 'Please log in' $YEAR/$DAY/input.txt; then
+  echo 'Error, please login and update your aoc session token'
+  exit 1
 fi
 
-pushd ../../../aoc-input
 git add $YEAR/$DAY
 git commit -am "$YEAR Day $DAY"
 git pushme
 popd
+
+ln -s ../../../aoc-input/$YEAR/$DAY/input.txt .
+if [ ! -e p.py ]; then
+  cp ../../p.py .
+fi
