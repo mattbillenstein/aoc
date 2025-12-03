22cd069417dbe305133a41776d66a51107d77e92
Matt Billenstein <matt@vazor.com>
2025-12-02 23:35:22 -0800

    Generalized case for parts 1 and 2

--- 2025/03/p.py
+++ 2025/03/p.py
@@ -2,58 +2,35 @@
 
 import sys
 
-DEBUG = sys.argv.count('-v')
-
-def debug(*args):
-    if DEBUG:
-        print(*args)
-
 def parse_input():
-    lines = [_.strip('\r\n') for _ in sys.stdin]
-    return (lines,)
+    return [_.strip('\r\n') for _ in sys.stdin]
 
-def part1(data):
-    # Find biggest digit except last, then biggest digit after that digit...
-    L = []
-    for bank in data:
-        a = max(bank[:-1])
-        idx = bank.find(a)
-        b = max(bank[idx+1:])
-        s = a + b
-
-        debug(bank, s)
-        L.append(int(s))
-
-    print(sum(L))
-
-def part2(data):
+def part(banks, N):
     # take the max digit in the part of the bank after our last batt turned on
-    # and the number of batts we need to make 12 digits...
-    L = []
-    for bank in data:
+    # and the number of batts we need to make N digits...
+    tot = 0
+    for bank in banks:
         # Negate the index here so we take the first max digit L to R order for
         # equal digits
         chars = [(c, -i) for i, c in enumerate(bank)]
 
         s = ''
-        i, j = 0, len(chars) - 11
-        for _ in range(12):
+        i = 0
+        for j in range(len(chars) - (N - 1), len(chars) + 1):
             c, idx = max(chars[i:j])
             s += c
-            i = -idx + 1  # undo negation
-            j += 1
+            i = -idx + 1  # undo negation here
 
-        debug(bank, s)
-        L.append(int(s))
+        tot += int(s)
 
-    print(sum(L))
+    print(tot)
 
 def main():
-    data = parse_input()
+    banks = parse_input()
     if '1' in sys.argv:
-        part1(*data)
+        part(banks, 2)
     if '2' in sys.argv:
-        part2(*data)
+        part(banks, 12)
 
 if __name__ == '__main__':
     main()
