0620ed2bb7513b51dd26680efb04fd41eef489fe
Matt Billenstein <matt@vazor.com>
2025-12-06 21:57:39 -0800

    Small cleanup

--- 2025/07/p.py
+++ 2025/07/p.py
@@ -11,27 +11,28 @@ def parse_input():
     return Grid(lines, 'S.|^')
 
 def part(g):
+    # trace each pt in grid down one step counting splits and building our
+    # adjacency list - this mutates the grid along the way...
     splits = 0
     graph = defaultdict(set)
-    for pt in g:
+    for pt in g:  # importantly iterates top row to bottom
         c = g.getc(pt)
         if c in ('S', '|'):
-            # Propagate straight down | to |
-            ppt = g.step(pt, '^')
-            if ppt and g.getc(ppt) == '|':
-                graph[ppt].add(pt)
-
             npt = g.step(pt, 'v')
             if npt:
                 nc = g.getc(npt)
                 if nc == '.':
                     g.setc(npt, '|')
                     graph[pt].add(npt)
+                elif nc == '|':
+                    # already got here, but add this to graph coming from other
+                    # direction...
+                    graph[pt].add(npt)
                 elif nc == '^':
                     splits += 1
                     for dir in ('<', '>'):
                         xpt = g.step(npt, dir)
-                        if xpt: # and g.getc(xpt) == '.':
+                        if xpt:
                             graph[pt].add(xpt)
                             g.setc(xpt, '|')
 
